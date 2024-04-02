d444f80ca553ccb8be7bd34a6ef4632167e66457
KIMHT98 <kht19684@gmail.com>
2024-04-02 22:35:45 +0900

    240402

--- 싸피/알고리즘/APS/src/day20_그래프비용2/최소비용구하기2_11779.java
+++ 싸피/알고리즘/APS/src/day20_그래프비용2/최소비용구하기2_11779.java
@@ -9,12 +9,11 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 public class 최소비용구하기2_11779 {
-	static class Node implements Comparable<Node>{
-		int start,end, w;
-		
-		public Node(int start, int end, int w) {
+	static class Node implements Comparable<Node> {
+		int end, w;
+
+		public Node(int end, int w) {
 			super();
-			this.start = start;
 			this.end = end;
 			this.w = w;
 		}
@@ -22,58 +21,82 @@ public class 최소비용구하기2_11779 {
 		@Override
 		public int compareTo(Node o) {
 			// TODO Auto-generated method stub
-			return this.w-o.w;
+			return this.w - o.w;
 		}
 	}
+
 	static int INF = Integer.MAX_VALUE;
-	static int N,M;
+	static int N, M,cnt;
 	static List<Node>[] adj;
-	static int[] dist;
+	static int[] dist,p;
+	static List<Integer> result;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
-		
-		adj = new ArrayList[N+1];
-		for(int i=1;i<=N;i++) {
+
+		adj = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++) {
 			adj[i] = new ArrayList<>();
 		}
-		dist = new int[N+1];
+		dist = new int[N + 1];
+		p = new int[N+1];
 		Arrays.fill(dist, INF);
-		
-		for(int i = 0;i<M;i++) {
+
+		for (int i = 0; i < M; i++) {
 			String[] arr = br.readLine().split(" ");
 			int start = Integer.parseInt(arr[0]);
 			int end = Integer.parseInt(arr[1]);
 			int w = Integer.parseInt(arr[2]);
-			adj[start].add(new Node(start,end,w));
+			adj[start].add(new Node(end, w));
 		}
 		String[] arr2 = br.readLine().split(" ");
 		int s = Integer.parseInt(arr2[0]);
 		int e = Integer.parseInt(arr2[1]);
-		dijkstra(s,e);
+		dijkstra(s, e);
 		System.out.println(dist[e]);
-		
+		cnt=1;
+		result = new ArrayList<>();
+		printPath(s, e);
+		System.out.println(result.size());
+		for(int i=result.size()-1;i>=0;i--) {
+			System.out.print(result.get(i)+" ");
+		}
+
 	}
-	static void dijkstra(int start,int end) {
+
+	static void dijkstra(int start, int end) {
 		PriorityQueue<Node> pq = new PriorityQueue<>();
-		boolean[] visit = new boolean[N+1];
+		boolean[] visit = new boolean[N + 1];
 		dist[start] = 0;
-		pq.add(new Node(start,start,0));
-		
-		while(!pq.isEmpty()) {
+		pq.add(new Node(start, 0));
+		while (!pq.isEmpty()) {
 			Node curr = pq.poll();
-			if(curr.end==end) {
+			if (curr.end == end) {
 				return;
 			}
-			if(visit[curr.end])continue;
-			visit[curr.end]= true;
-			for(Node node : adj[curr.end]) {
-				if(!visit[node.end] && dist[node.end]>dist[curr.end]+node.w) {
-					dist[node.end] = dist[curr.end]+node.w;
-					pq.add(new Node(node.start,node.end,dist[node.end]));
+			
+			if (visit[curr.end])
+				continue;
+			visit[curr.end] = true;
+			for (Node node : adj[curr.end]) {
+				if (!visit[node.end]) {
+					if (dist[node.end] > dist[curr.end] + node.w) {
+						dist[node.end] = dist[curr.end] + node.w;
+						pq.add(new Node(node.end, dist[node.end]));
+						p[node.end] = curr.end;
+					}
 				}
 			}
 		}
 	}
+	  static void printPath(int start, int end) {
+	        if (end == start) {
+	            result.add(end);
+	            return;
+	        }
+	        result.add(end);
+	        printPath(start, p[end]);
+	    }
+	
 }
