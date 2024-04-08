bc35bf68fb6520fd500db53cca83d668c7284baa
KIMHT98 <kht19684@gmail.com>
2024-04-08 23:31:06 +0900

    240408

--- 싸피/알고리즘/APS/src/A형대비/핀볼.java
+++ 싸피/알고리즘/APS/src/A형대비/핀볼.java
@@ -1,24 +1,109 @@
 package A형대비;
 
 public class 핀볼 {
-	static int N;
+	static int N,max;
 	static int[][] board;
-	static int[] dr= {-1,1,0,0};
-	static int[] dc = {0,0,-1,1};
-	static class ball{
-		int row,col,point;
+	static int[] dr = { -1, 0, 1, 0 };
+	static int[] dc = { 0, 1, 0, -1 };
 
-		public ball(int row, int col, int point) {
+	static class ball {
+		int row, col, point, dir;
+
+		public ball(int row, int col, int point, int dir) {
 			this.row = row;
 			this.col = col;
 			this.point = point;
+			this.dir = dir;
 		}
-		
 	}
-//	static int pinball(int srow, int scol, int direct) {
-//		
-////		if(ball.row==)
-//	}
+
+	static int changedir1(int i) {
+		switch (i) {
+		case 0:
+			return 2;
+		case 1:
+			return 3;
+		case 2:
+			return 1;
+		case 3:
+			return 0;
+		default:
+			return -1;
+		}
+	}
+	static int changedir2(int i) {
+		switch (i) {
+		case 0:
+			return 1;
+		case 1:
+			return 3;
+		case 2:
+			return 0;
+		case 3:
+			return 2;
+		default:
+			return -1;
+		}
+	}
+	static int changedir3(int i) {
+		switch (i) {
+		case 0:
+			return 3;
+		case 1:
+			return 2;
+		case 2:
+			return 0;
+		case 3:
+			return 1;
+		default:
+			return -1;
+		}
+	}
+	static int changedir4(int i) {
+		switch (i) {
+		case 0:
+			return 2;
+		case 1:
+			return 0;
+		case 2:
+			return 3;
+		case 3:
+			return 1;
+		default:
+			return -1;
+		}
+	}
+	static int changedir5(int i) {
+		switch (i) {
+		case 0:
+			return 2;
+		case 1:
+			return 3;
+		case 2:
+			return 0;
+		case 3:
+			return 1;
+		default:
+			return -1;
+		}
+	}
+
+	static void pinball(int srow, int scol) {
+		for (int i = 0; i < 4; i++) {
+			ball b = new ball(srow,scol , 0, i);
+			int nr = srow;
+			int nc = scol;
+			while(nr!=srow || nc!=scol) {
+				nr+=dr[b.dir];
+				nc+=dc[b.dir];
+				if(nr==0)
+			if(nr==srow && nc==scol) {
+				max = Math.max(max, b.point);
+				return;
+			}
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
