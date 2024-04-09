f5ee92c51cc156237e001175fc06a411adcd8e23
KIMHT98 <kht19684@gmail.com>
2024-04-09 16:58:33 +0900

    240409

--- 싸피/알고리즘/APS/src/A형대비/핀볼.java
+++ 싸피/알고리즘/APS/src/A형대비/핀볼.java
@@ -1,8 +1,13 @@
 package A형대비;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class 핀볼 {
-	static int N,max;
+	static int N, max;
 	static int[][] board;
+	static List<Integer>[] hole;
 	static int[] dr = { -1, 0, 1, 0 };
 	static int[] dc = { 0, 1, 0, -1 };
 
@@ -31,6 +36,7 @@ public class 핀볼 {
 			return -1;
 		}
 	}
+
 	static int changedir2(int i) {
 		switch (i) {
 		case 0:
@@ -45,6 +51,7 @@ public class 핀볼 {
 			return -1;
 		}
 	}
+
 	static int changedir3(int i) {
 		switch (i) {
 		case 0:
@@ -59,6 +66,7 @@ public class 핀볼 {
 			return -1;
 		}
 	}
+
 	static int changedir4(int i) {
 		switch (i) {
 		case 0:
@@ -73,6 +81,7 @@ public class 핀볼 {
 			return -1;
 		}
 	}
+
 	static int changedir5(int i) {
 		switch (i) {
 		case 0:
@@ -88,26 +97,114 @@ public class 핀볼 {
 		}
 	}
 
+	static int[] meethole(int num, int row, int col) {
+		int[] newposition = new int[2];
+		if (row == hole[num - 6].get(0)&&col==hole[num-6].get(1)) {
+			newposition[0] = hole[num - 6].get(2);
+			newposition[1] = hole[num - 6].get(3);
+		}  
+		if(row == hole[num - 6].get(2)&&col==hole[num-6].get(3)){
+			newposition[0] = hole[num - 6].get(0);
+			newposition[1] = hole[num - 6].get(1);
+		}
+		return newposition;
+	}
+
 	static void pinball(int srow, int scol) {
 		for (int i = 0; i < 4; i++) {
-			ball b = new ball(srow,scol , 0, i);
+			ball b = new ball(srow, scol, 0, i);
 			int nr = srow;
 			int nc = scol;
-			while(nr!=srow || nc!=scol) {
-				nr+=dr[b.dir];
-				nc+=dc[b.dir];
-				if(nr==0)
-			if(nr==srow && nc==scol) {
-				max = Math.max(max, b.point);
-				return;
-			}
+			while (true) {
+				nr += dr[b.dir];
+				nc += dc[b.dir];
+				if (nr < 0 || nr >= N || nc < 0 || nc >= N) {
+					b.dir = changedir5(b.dir);
+					b.point++;
+//					nr += dr[b.dir];
+//					nc += dc[b.dir];
+				} else {
+//					System.out.println(nr+" "+nc);
+					if (board[nr][nc] == -1) {
+						max = Math.max(max, b.point);
+						break;
+					}
+					if (board[nr][nc] == 1) {
+						b.dir = changedir1(b.dir);
+						b.point++;
+					} else if (board[nr][nc] == 2) {
+						b.dir = changedir2(b.dir);
+						b.point++;
+					} else if (board[nr][nc] == 3) {
+						b.dir = changedir3(b.dir);
+						b.point++;
+					} else if (board[nr][nc] == 4) {
+						b.dir = changedir4(b.dir);
+						b.point++;
+					} else if (board[nr][nc] == 5) {
+						b.dir = changedir5(b.dir);
+						b.point++;
+					} else if (board[nr][nc] > 5) {
+						int[] newposition = meethole(board[nr][nc], nr, nc);
+						nr = newposition[0];
+						nc = newposition[1];
+//						System.out.println(nr+" "+nc);
+					}
+				}
+				b.row = nr;
+				b.col = nc;
+//				System.out.println(b.row + " " + b.col+" "+b.point);
+				if (b.row == srow && b.col == scol) {
+					max = Math.max(max, b.point);
+					break;
+				}
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int t = 1; t <= T; t++) {
+			N = sc.nextInt();
+			board = new int[N][N];
+			hole = new ArrayList[5];
+			for (int i = 0; i < 5; i++) {
+				hole[i] = new ArrayList<>();
+			}
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+					board[r][c] = sc.nextInt();
+					if (board[r][c] > 5) {
+						hole[board[r][c] - 6].add(r);
+						hole[board[r][c] - 6].add(c);
+					}
+				}
+			}
+//			for(List<Integer> list:hole) {
+//				System.out.println(list.toString());
+//			}
+			max=0;
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+					if (board[r][c] == 0) {
+//						System.out.println(r+" "+c);
+						pinball(r, c);
+					}
+				}
+			}
+			System.out.println("#" + t + " " + max);
 
+		}
 	}
 
 }
+/*
+ 1
+5
+0 6 7 6 7
+5 5 5 5 5
+5 5 5 5 5
+5 5 5 5 5
+5 5 5 5 5
+ */
