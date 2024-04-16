8b37020a5e8ec4c89b9f5dc3840044a48965e264
KIMHT98 <kht19684@gmail.com>
2024-04-16 18:05:21 +0900

    240416

--- 싸피/알고리즘/백준class문제/src/class3/잃어버린괄호.java
+++ 싸피/알고리즘/백준class문제/src/class3/잃어버린괄호.java
@@ -1,10 +1,30 @@
 package class3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class 잃어버린괄호 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		String[] cal = sc.next().split("-");
+		for(int i=0;i<cal.length;i++) {
+			if(cal[i].contains("+")) {
+				int num = 0;
+				String[] plus = cal[i].split("\\+");
+				for(int j=0;j<plus.length;j++) {
+					num+= Integer.parseInt(plus[j]);
+				}
+				cal[i] = num +"";
+			}
+		}
+		int result = Integer.parseInt(cal[0]);
+		for(int i=1;i<cal.length;i++) {
+			result-=Integer.parseInt(cal[i]);
+		}
+		System.out.println(result);
 	}
 
 }
