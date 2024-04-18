004c426c666ee8a399742883de28459d91c8e755
KIMHT98 <kht19684@gmail.com>
2024-04-18 09:07:05 +0900

    240418

---
+++ 싸피/알고리즘/백준class문제/src/class3/가장가까운세사람의심리적거리.java
@@ -0,0 +1,17 @@
+package class3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 가장가까운세사람의심리적거리 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int t=0;t<T;t++) {
+			
+		}
+	}
+
+}
--- 싸피/알고리즘/백준class문제/src/class3/좌표압축.java
+++ 싸피/알고리즘/백준class문제/src/class3/좌표압축.java
@@ -1,7 +1,8 @@
 package class3;
 
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class 좌표압축 {
@@ -9,31 +10,21 @@ public class 좌표압축 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
+		Map<Integer,Integer> map = new HashMap<>();
 		int n = sc.nextInt();
-		int[][] points = new int[n][2];
-		int[] result = new int[n];
+		int[] origin = new int[n];
+		int[] points = new int[n];
 		for (int i = 0; i < n; i++) {
-			points[i][0] = sc.nextInt();
-			points[i][1] = i;
+			points[i] = sc.nextInt();
+			origin[i] = points[i];
 		}
-		Arrays.sort(points, new Comparator<int[]>() {
-
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				// TODO Auto-generated method stub
-				return o2[0] - o1[0];
-			}
-		});
-		result[points[0][1]] = n-1;
-		for (int i = 1; i < n; i++) {
-			result[points[i][1]] = n - 1 - i;
-			if (points[i][0] == points[i - 1][0]) {
-				result[points[i][1]] = result[points[i - 1][1]];
-			}
-			
+		points = Arrays.stream(points).distinct().toArray();
+		Arrays.sort(points);
+		for(int i=points.length-1;i>=0;i--) {
+			map.put(points[i], i);
 		}
 		for (int i = 0; i < n; i++) {
-			sb.append(result[i]).append(" ");
+			sb.append(map.get(origin[i])).append(" ");
 		}
 		System.out.println(sb);
 	}
