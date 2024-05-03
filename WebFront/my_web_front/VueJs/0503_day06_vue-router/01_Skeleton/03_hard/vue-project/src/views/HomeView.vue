ab11364409448be55ebf4e80201f036adcd42a41
kht19684 <kht19684@gmail.com>
2024-05-03 11:36:08 +0900

    240503

---
+++ WebFront/my_web_front/VueJs/0503_day06_vue-router/01_Skeleton/03_hard/vue-project/src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <div><h1>HomeView입니다.</h1></div>
+</template>
---
+++ WebFront/my_web_front/VueJs/0503_day06_vue-router/01_Skeleton/03_hard/vue-project/src/views/UserView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>UserView입니다.</h1>
+
+    <h3>{{ userId }}번 User페이지</h3>
+    <button @click="goHome">홈으로</button>
+    <button @click="banKok">방콕</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const userId = ref(route.params.id);
+const router = useRouter();
+const goHome = () => {
+  router.push({ name: "home" });
+};
+const banKok = () => {
+  router.replace({ name: "home" });
+};
+</script>
+
+<style scoped></style>
